7b68777aa66c194c97c8ffea478d9ee0ad4bd2e8
GJ-jirayu <gj-jirayu@admin@gitlab.com>
2015-07-14 17:28:55 +0700

    king commit 14072558

---
+++ Database/export_DIM_BALANCE_TYPE.sql
@@ -0,0 +1,8 @@
+--------------------------------------------------------
+--  File created - Wednesday-April-22-2015   
+--------------------------------------------------------
+REM INSERTING into EXAT_DWH.DIM_BALANCE_TYPE
+SET DEFINE OFF;
+Insert into EXAT_DWH.DIM_BALANCE_TYPE (BALANCE_TYPE_KEY,BALANCE_TYPE_NAME,CREATED_DTTM,UPDATED_DTTM) values (1,'บัตรเงินติดลบ',to_timestamp('24-OCT-14 03.11.26.000000000 PM','DD-MON-RR HH.MI.SS.FF AM'),to_timestamp('24-OCT-14 03.11.26.000000000 PM','DD-MON-RR HH.MI.SS.FF AM'));
+Insert into EXAT_DWH.DIM_BALANCE_TYPE (BALANCE_TYPE_KEY,BALANCE_TYPE_NAME,CREATED_DTTM,UPDATED_DTTM) values (2,'บัตรเงินเป็นศูนย์',to_timestamp('24-OCT-14 03.22.25.751000000 PM','DD-MON-RR HH.MI.SS.FF AM'),to_timestamp('24-OCT-14 03.22.25.751000000 PM','DD-MON-RR HH.MI.SS.FF AM'));
+Insert into EXAT_DWH.DIM_BALANCE_TYPE (BALANCE_TYPE_KEY,BALANCE_TYPE_NAME,CREATED_DTTM,UPDATED_DTTM) values (3,'บัตรมีเงินคงเหลือ',to_timestamp('24-OCT-14 03.23.23.829000000 PM','DD-MON-RR HH.MI.SS.FF AM'),to_timestamp('24-OCT-14 03.23.23.829000000 PM','DD-MON-RR HH.MI.SS.FF AM'));
---
+++ Database/export_HISHWAY_SHORTNAME_.sql
@@ -0,0 +1,10 @@
+--------------------------------------------------------
+--  File created - Wednesday-April-22-2015   
+--------------------------------------------------------
+REM INSERTING into EXAT_DWH.HIGHWAY_SHORTNAME_TEMPLATE
+SET DEFINE OFF;
+Insert into EXAT_DWH.HIGHWAY_SHORTNAME_TEMPLATE (HIGHWAY_CODE,HIGHWAY_SHORTNAME) values ('01','เฉลิมมหานคร');
+Insert into EXAT_DWH.HIGHWAY_SHORTNAME_TEMPLATE (HIGHWAY_CODE,HIGHWAY_SHORTNAME) values ('02','ศรีรัช');
+Insert into EXAT_DWH.HIGHWAY_SHORTNAME_TEMPLATE (HIGHWAY_CODE,HIGHWAY_SHORTNAME) values ('03','ฉลองรัช');
+Insert into EXAT_DWH.HIGHWAY_SHORTNAME_TEMPLATE (HIGHWAY_CODE,HIGHWAY_SHORTNAME) values ('04','บูรพาวิถี');
+Insert into EXAT_DWH.HIGHWAY_SHORTNAME_TEMPLATE (HIGHWAY_CODE,HIGHWAY_SHORTNAME) values ('06','กาญจนาภิเษก');
